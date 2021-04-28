6ebacdd2d68eeed2cb75f8355734ca991132522f
Maninisp <47589590+Maninisp@users.noreply.github.com>
2021-04-28 15:53:51 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,42 @@
+function add(){
+    var item = document.getElementById("item").value;
+
+    if (item.length < 28){
+    var div = document.createElement("div");
+
+    var t = document.createTextNode(item);
+    var button = document.createElement("button");
+    button.innerHTML = "X";
+    
+    button.addEventListener("click", del);
+    function del(){
+        var div = this.parentElement;
+        div.parentNode.removeChild(div);
+    }
+    div.appendChild(t);
+    div.appendChild(button);
+
+    document.getElementById("display").appendChild(div)
+
+   document.getElementById("item").value = "";
+   }
+
+   else{
+    alert("max. 28 characters only");
+   }
+
+}
+// var item = document.getElementById("item").value;
+// if (item.length> 28){
+    
+    
+
+// }
+
+// function add() {
+//     var x = document.createElement("div");
+//     var tk = document.getElementById("item").value;
+//     var t = document.createTextNode(tk);
+//     x.appendChild(t);
+//     document.getElementById("myList").appendChild(x);
+//   }
